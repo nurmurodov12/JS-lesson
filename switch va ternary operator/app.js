ea24bcdf26c6f205c729156b928906b5c6e98d83
nurmurodov12 <nurmurodovtemur86@gmail.com>
2025-11-27 23:09:13 +0500

    man yana yangi dars code qoshdim

--- switch va ternary operator/app.js
+++ switch va ternary operator/app.js
@@ -39,13 +39,13 @@
 
 // if (fruit === "banan") {
 //     console.log("BU banan");
-    
+
 // } else if (fruit ==="olma" ){
 //      console.log("bu olma");
 
 // }else if (fruit === "gilos") {
 //     console.log("bu gilos");
-    
+
 // }else {
 //     console.log("meva turi aniqlamadi");
 
@@ -61,8 +61,86 @@
 // const message = isLoggedIn ? "welcome back" : "please log in"  ;
 // console.log(message);
 
+
 // const age = 18;
 // const result = age >= 18 ? "yes i true" : "yo try not"
 // console.log(result);
 
+// const natijalar = [30, 43, 0, 65, 100, 20, 11];
 
+// for (let i = 0; i < natijalar.length; i++) {
+
+//     if(natijalar[i] === 0) {
+//         continue
+//     }
+
+//     console.log("sizning natijalaringiz :", natijalar[i]);
+
+//     if (natijalar[i] === 100); {
+//         console.log("siz eng yuqori natijani to'pladingz");
+//         break
+
+//     }
+
+
+
+
+// }
+
+
+
+// const baho = "D";
+// switch (baho) {
+//     case 'A':
+//         console.log("sizning bahoyingiz A");
+//         break
+//     case 'D':
+//         console.log("sizning bahoyingiz B");
+//         break
+//     case 'C':
+//         console.log("sizning bahoyingiz C");
+//         break
+//     case 'D':
+//         console.log("sizngning bahoyingiz D");
+//         break
+//     default:
+//         console.log("sizning bahoyingiz bebaho");
+
+// }
+
+
+// let age = 24;
+
+// if (true) {
+//     let ism = "temur"
+//     let age = 25;
+//     console.log("ifni ichida elon qilingan age", age, ism);
+
+//     if(true) {
+//         // let age = 26;
+//         console.log("bu esa ifni ichida yana if",age, ism);
+        
+//     }
+    
+// }
+
+// console.log("tashqarida elon qilingan age ", age);
+
+let yosh = 20
+switch (true) {
+    case yosh > 18:
+        console.log("bu bolani yoshi 18dan katta ");
+        break
+    case yosh === 18:
+        console.log("bu bola yoshi 18da");
+        break
+    case yosh === 20:
+        console.log("bu bolani yoshi 20da");
+        break
+    default:
+        console.log("bu bolani yoshi nexuya topilmadi");
+        
+        
+        
+        
+}
