8ca6c1e4e09a9eb6eddd09fdfd8fe0e3425d8f25
nurmurodov12 <nurmurodovtemur86@gmail.com>
2025-12-07 12:44:43 +0500

    js

--- function experesion va arrow function/app.js
+++ function experesion va arrow function/app.js
@@ -1,27 +1,27 @@
 // Function expersion
 // const funksiya = function () {
 //     console.log("salom dunyo");
-    
+
 // }
 
 // funksiya();
 
 // const add = function (a , b) {
 //     return a + b;
-     
+
 // } ;
 // console.log(add(5, 7));
 
 // const sayHEllo = function add(a, b) {
 //     return a + b
-    
+
 // }
 
 // console.log(sayHEllo(5, 7));
 
 // const sayHello = function ( ism = "dost") {
 //     console.log(`salom, ${ism}!`);
-    
+
 // };
 // sayHello('sarvar');
 // sayHello("Temur");
@@ -30,7 +30,7 @@
 
 // const sayHello = () => {
 //     console.log("salom dunyo!");
-    
+
 // };
 // sayHello();
 
@@ -84,14 +84,39 @@
 //     return total
 // }
 
-const bill = (products, tax) => {
-    let total = 0
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax
-    }
+// const bill = (products, tax) => {
+//     let total = 0
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax
+//     }
 
-    return total
-}
+//     return total
+// }
 
-const result = bill([10, 13, 45], 0.2)
-console.log(result);
+// const result = bill([10, 13, 45], 0.2)
+// console.log(result);
+
+// const myFunc = (callBackFunc) => {
+//     let number = 50
+//     callBackFunc(number)
+// }
+
+// myFunc(function(value) {
+//     console.log(value);
+    
+// })
+
+// for each
+
+
+const ism = ["ahror", "sardor", "doniyor", "temur",]
+ism.forEach(function(ism) {
+    const newIsm = ism
+})
+
+
+
+// for (let i = 0; i < ism.length; i++) {
+//     console.log(ism[i]);
+    
+// }
